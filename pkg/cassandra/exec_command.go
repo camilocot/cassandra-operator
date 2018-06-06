94ccbec99ecba6dbdfd1c4136029504571cefc8b
Camilo <camilocot@users.noreply.github.com>
2018-06-06 20:49:55 +0200

    Move cassandra cluster reconciliation to an separate package (#1)

---
+++ pkg/cassandra/exec_command.go
@@ -0,0 +1,121 @@
+package cassandra
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/camilocot/cassandra-operator/pkg/apis/database/v1alpha1"
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
+	"github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
+)
+
+func ExecCommand(c *v1alpha1.Cassandra, podName string, command ...string) (string, error) {
+
+	var (
+		execOut bytes.Buffer
+		execErr bytes.Buffer
+	)
+
+	pod := &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: c.Namespace,
+		},
+	}
+
+	err := sdk.Get(pod)
+	if err != nil {
+		return "", fmt.Errorf("could not get pod info: %v", err)
+	}
+
+	if len(pod.Spec.Containers) != 1 {
+		return "", fmt.Errorf("could not determine which container to use")
+	}
+
+	kubeClient, kubeConfig := mustNewKubeClientAndConfig()
+
+	req := kubeClient.CoreV1().RESTClient().Post().
+		Resource("pods").
+		Name(pod.Name).
+		Namespace(pod.Namespace).
+		SubResource("exec")
+	req.VersionedParams(&v1.PodExecOptions{
+		Container: pod.Spec.Containers[0].Name,
+		Command:   command,
+		Stdout:    true,
+		Stderr:    true,
+	}, scheme.ParameterCodec)
+
+	exec, err := remotecommand.NewSPDYExecutor(kubeConfig, "POST", req.URL())
+	if err != nil {
+		return "", fmt.Errorf("failed to init executor: %v", err)
+	}
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdout: &execOut,
+		Stderr: &execErr,
+		Tty:    false,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("could not execute: %v", err)
+	}
+
+	if execErr.Len() > 0 {
+		return "", fmt.Errorf("stderr: %v", execErr.String())
+	}
+
+	return execOut.String(), nil
+}
+
+// mustNewKubeClientAndConfig returns the in-cluster config and kubernetes client
+// or if KUBERNETES_CONFIG is given an out of cluster config and client
+func mustNewKubeClientAndConfig() (kubernetes.Interface, *rest.Config) {
+	var cfg *rest.Config
+	var err error
+	if os.Getenv(k8sutil.KubeConfigEnvVar) != "" {
+		cfg, err = outOfClusterConfig()
+	} else {
+		cfg, err = inClusterConfig()
+	}
+	if err != nil {
+		panic(err)
+	}
+	return kubernetes.NewForConfigOrDie(cfg), cfg
+}
+
+// inClusterConfig returns the in-cluster config accessible inside a pod
+func inClusterConfig() (*rest.Config, error) {
+	// Work around https://github.com/kubernetes/kubernetes/issues/40973
+	// See https://github.com/coreos/etcd-operator/issues/731#issuecomment-283804819
+	if len(os.Getenv("KUBERNETES_SERVICE_HOST")) == 0 {
+		addrs, err := net.LookupHost("kubernetes.default.svc")
+		if err != nil {
+			return nil, err
+		}
+		os.Setenv("KUBERNETES_SERVICE_HOST", addrs[0])
+	}
+	if len(os.Getenv("KUBERNETES_SERVICE_PORT")) == 0 {
+		os.Setenv("KUBERNETES_SERVICE_PORT", "443")
+	}
+	return rest.InClusterConfig()
+}
+
+func outOfClusterConfig() (*rest.Config, error) {
+	kubeconfig := os.Getenv(k8sutil.KubeConfigEnvVar)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	return config, err
+}
