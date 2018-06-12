a3f7740009f9f5e5f77b239d4c8991b57da793a8
Camilo Cota <camilocot@gmail.com>
2018-06-12 21:22:15 +0200

    updateStatefulset test

---
+++ pkg/cassandra/reconcile_test.go
@@ -0,0 +1,24 @@
+package cassandra
+
+import (
+	"testing"
+
+	api "github.com/camilocot/cassandra-operator/pkg/apis/database/v1alpha1"
+)
+
+func TestUpdateStatefulset(t *testing.T) {
+	c := api.Cassandra{
+		Spec: api.CassandraSpec{
+			Partition:  1,
+			Repository: "repo1",
+			Size:       1,
+		},
+	}
+	s := statefulsetForCassandra(&c)
+
+	c.Spec.Partition = 2
+	s = updateStatefulset(&c, s)
+	if &c.Spec.Partition != s.Spec.UpdateStrategy.RollingUpdate.Partition {
+		t.Error("Partition wasn't updated")
+	}
+}
