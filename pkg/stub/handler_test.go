45272d5e057a64b609d7cf792d8aecd7ed11c7f2
Camilo Cota <camilocot@gmail.com>
2018-06-24 12:14:46 +0200

    Add UT for Reconcile Cassandra cluster

--- pkg/stub/handler_test.go
+++ pkg/stub/handler_test.go
@@ -2,6 +2,8 @@ package stub
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"testing"
 
 	v1alpha1 "github.com/camilocot/cassandra-operator/pkg/apis/database/v1alpha1"
@@ -46,8 +48,8 @@ func (m *MockCassandaCluster) SetDefaults() bool {
 }
 
 func (m *MockCassandaCluster) FailedReconciliation(failedObjectName string, err error) error {
-	args := m.Called(failedObjectName, err)
-	return args.Error(0)
+	_ = m.Called(failedObjectName, err)
+	return fmt.Errorf("%s %v", failedObjectName, err)
 }
 
 func (suite *HandlerTestSuite) SetupTest() {
@@ -95,6 +97,72 @@ func (suite *HandlerTestSuite) TestReconcileWithValidInput() {
 	cluster.AssertExpectations(suite.T())
 	assert.Nil(suite.T(), err)
 	assert.True(suite.T(), probe.GetReady())
+}
+
+func (suite *HandlerTestSuite) TestReconcileWithServiceFailure() {
+	err := errors.New("failed")
+	cluster := new(MockCassandaCluster)
+	cluster.On("SetDefaults").Return(false)
+	cluster.On("ReconcileService").Return(err)
+	cluster.On("FailedReconciliation", "service", err).Return(nil)
+
+	handler := NewHandler()
+	err = handler.Reconcile(cluster)
+
+	cluster.AssertExpectations(suite.T())
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), "service failed", err.Error())
+}
+
+func (suite *HandlerTestSuite) TestReconcileWithMembersFailure() {
+	err := errors.New("failed")
+	cluster := new(MockCassandaCluster)
+	cluster.On("SetDefaults").Return(false)
+	cluster.On("ReconcileService").Return(nil)
+	cluster.On("ReconcileMembers").Return(errors.New("failed"))
+	cluster.On("FailedReconciliation", "members", err).Return(nil)
+
+	handler := NewHandler()
+	err = handler.Reconcile(cluster)
+
+	cluster.AssertExpectations(suite.T())
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), "members failed", err.Error())
+}
+
+func (suite *HandlerTestSuite) TestReconcileWithStatefulsetFailure() {
+	err := errors.New("failed")
+	cluster := new(MockCassandaCluster)
+	cluster.On("SetDefaults").Return(false)
+	cluster.On("ReconcileService").Return(nil)
+	cluster.On("ReconcileMembers").Return(nil)
+	cluster.On("ReconcileStatefulset").Return(errors.New("failed"))
+	cluster.On("FailedReconciliation", "statefulset", err).Return(nil)
+
+	handler := NewHandler()
+	err = handler.Reconcile(cluster)
+
+	cluster.AssertExpectations(suite.T())
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), "statefulset failed", err.Error())
+}
+
+func (suite *HandlerTestSuite) TestReconcileWithStatusFailure() {
+	err := errors.New("failed")
+	cluster := new(MockCassandaCluster)
+	cluster.On("SetDefaults").Return(false)
+	cluster.On("ReconcileService").Return(nil)
+	cluster.On("ReconcileMembers").Return(nil)
+	cluster.On("ReconcileStatefulset").Return(nil)
+	cluster.On("ReconcileStatus").Return(errors.New("failed"))
+	cluster.On("FailedReconciliation", "status", err).Return(nil)
+
+	handler := NewHandler()
+	err = handler.Reconcile(cluster)
+
+	cluster.AssertExpectations(suite.T())
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), "status failed", err.Error())
 }
 
 // Run test suite...
